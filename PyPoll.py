a769f0aec4c93835762f5e3bd46dad3a7c64e053
jcsargis00 <79231870+jcsargis00@users.noreply.github.com>
2022-01-15 22:01:46 -0800

    update

--- PyPoll.py
+++ PyPoll.py
@@ -77,3 +77,5 @@ with open(file_to_save, "w") as txt_file:
         f"-------------------------\n")
 
     #print(winning_candidate_summary)
+    # Save the winning candidate's name to the text file.
+    txt_file.write(winning_candidate_summary)
