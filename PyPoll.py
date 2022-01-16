329988d4d9e2ff4b9ea1a7f280475fc831e8adb4
jcsargis00 <79231870+jcsargis00@users.noreply.github.com>
2022-01-15 20:28:50 -0800

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -4,22 +4,39 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 # Assign a variable for the file to load and the path.
-
+# Add our dependencies.
 import csv
 import os
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-
-    # Print the file object.
-     print(election_data)
-
-# Create a filename variable to a direct or indirect path to the file.
+# Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Using the with statement open the file as a text file. 
-with open(file_to_save, "w") as txt_file:
+# Initialize a total vote counter.
+total_votes = 0
 
-      # Write three counties to the file.
-      txt_file.write("Counties in the Election\n-----------------------\nArapahoe\nDenver\nJefferson")
+# Candidate Options
+candidate_options = []
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)
