11b909eac4932b0e52fd9806b9e823f67df1083b
jcsargis00 <79231870+jcsargis00@users.noreply.github.com>
2022-01-15 19:41:13 -0800

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -18,7 +18,7 @@ with open(file_to_load) as election_data:
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Using the with statement open the file as a text file.
+# Using the with statement open the file as a text file. 
 with open(file_to_save, "w") as txt_file:
 
       # Write three counties to the file.
